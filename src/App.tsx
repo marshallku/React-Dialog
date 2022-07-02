e99d99289ba28ef3d1589034028092658cc1a492
marshallku <marshall@kakao.com>
2022-07-02 16:59:17 +0900

    Add examples to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,35 @@
+import { useCallback, useState } from "react";
+import useDialog from "./hooks/useDialog";
+
 export default function App() {
-    return <div>Hello world!</div>;
+    const { confirm, alert } = useDialog();
+    const [message, setMessage] = useState("");
+
+    const showConfirm = useCallback(async () => {
+        const confirmed = await confirm("Are you sure?");
+
+        if (confirmed) {
+            setMessage("Sure!");
+        } else {
+            setMessage("Nope.");
+        }
+    }, []);
+
+    const showAlert = useCallback(async () => {
+        alert("Hello there!");
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <button type="button" onClick={showConfirm}>
+                    Confirm
+                </button>
+                <button type="button" onClick={showAlert}>
+                    Alert
+                </button>
+                <div>{message}</div>
+            </div>
+        </div>
+    );
 }
