b26a239dc563f42a0994df4ebbc76baf733393c5
marshallku <marshall@kakao.com>
2022-07-03 13:39:30 +0900

    Update design of dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,6 +1,7 @@
 import { createPortal } from "react-dom";
 import useDialogStore from "../store/useDialogStore";
 import useDialog from "../hooks/useDialog";
+import "./Dialog.css";
 
 export default function Dialog() {
     const dialogRoot = document.getElementById("dialog") as HTMLElement;
@@ -8,20 +9,30 @@ export default function Dialog() {
     const { onConfirm, onCancel } = useDialog();
 
     const DialogComponent = () => (
-        <section className="dialog">
-            <div className="dialog__backdrop" />
-            <h2>{text}</h2>
-            <div className="dialog__buttons">
-                <button type="button" onClick={onConfirm}>
-                    OK
-                </button>
-                {type === "confirm" && (
-                    <button type="button" onClick={onCancel}>
-                        Cancel
+        <>
+            <div className="dialog-backdrop" />
+            <section className="dialog">
+                <h2 className="dialog__title">{text}</h2>
+                <div className="dialog__buttons">
+                    <button
+                        type="button"
+                        className="dialog__button dialog__button--confirm"
+                        onClick={onConfirm}
+                    >
+                        OK
                     </button>
-                )}
-            </div>
-        </section>
+                    {type === "confirm" && (
+                        <button
+                            type="button"
+                            className="dialog__button dialog__button--cancel"
+                            onClick={onCancel}
+                        >
+                            Cancel
+                        </button>
+                    )}
+                </div>
+            </section>
+        </>
     );
 
     return createPortal(revealed ? <DialogComponent /> : null, dialogRoot);
